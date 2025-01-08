c6ad234265438f9e576a34ca2d8637d60068685e
giosar-08 <giorgio.saracino@marconibari.edu.it>
2025-01-08 16:38:42 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
-#include <iostream>
-using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    std::string V[5];
+    V[0] = "Matt";
+    V[1] = "Amy";
+    V[2] = "Rick";
+    V[3] = "Alex";
+    V[4] = "David";
+int main() {
+    int a = 0;
+  
+    while (a != 5) { 
+        std::cout << "nome" << std::endl;
+        int n;
+        std::cin >> n;
+int main() {
+    int a = 1;
+    std::vector<int> x(a);
+
+    while (a != 10) {
+        std::cout << a << std::endl;
+        a = a + 10;
+    int a = 1;
+    std::vector<int> x(a);
+
+    while (a < 10) {
+        if (a % 2 == 0) {
+            std::cout << a << std::endl;
+        }
+  
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+
